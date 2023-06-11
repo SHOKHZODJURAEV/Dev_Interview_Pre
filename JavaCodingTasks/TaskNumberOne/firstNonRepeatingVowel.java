9cd9548448815074d2d2dd4e1028c282bf6cdf5b
SHOKHZODJURAEV <85317197+SHOKHZODJURAEV@users.noreply.github.com>
2023-06-11 10:08:09 -0400

    add some description.

--- JavaCodingTasks/TaskNumberOne/firstNonRepeatingVowel.java
+++ JavaCodingTasks/TaskNumberOne/firstNonRepeatingVowel.java
@@ -12,12 +12,12 @@ public class firstNonRepeatingVowel {
     }
 
     public static int findFirstNonRepeatingVowel(String target){
-
+        // first step make string to lowercase and store to char array
         char[] targetCharacters = target.toLowerCase().toCharArray();
         char[] vowelCharacters = {'a', 'e', 'i', 'o', 'u'};
         int currentIndex = -1;
         int firstVowelIndex = Integer.MAX_VALUE;
-
+        // second step iterate each vowel character to target char array value
         for(char eachVowel : vowelCharacters) {
             int vowelCounter = 0;
             for(char eachCharacter : targetCharacters){
@@ -25,13 +25,16 @@ public class firstNonRepeatingVowel {
                     vowelCounter++;
                 }
             }
+            // third step defining only one vowel character in target char array value
             if(vowelCounter == 1 ){
                  currentIndex = target.toLowerCase().indexOf(eachVowel);
+                 // fourth find out first vowel character in the target String value
                 if( firstVowelIndex > currentIndex) {
                     firstVowelIndex = currentIndex;
                  }
                 }
             }
+        // return first vowel or -1 if there is not any unique vowel character;
         return firstVowelIndex > target.length()? -1 : firstVowelIndex;
         }
     }
