b225c44c93db6e986e83acb04ec12c9872f80213
SHOKHZODJURAEV <85317197+SHOKHZODJURAEV@users.noreply.github.com>
2023-06-23 21:24:48 -0400

    second task done. Added null pointer exception

--- JavaCodingTasks/TaskNumberTwo/JadenCasingStrings.java
+++ JavaCodingTasks/TaskNumberTwo/JadenCasingStrings.java
@@ -6,20 +6,25 @@ public class JadenCasingStrings {
 
     public static void main(String[] args) {
 
-        String sentence = "     how can   mirrors    be real if    our eyes   aren't   real     ";
+        String sentence = null;
 
-       // System.out.println(jadenSmithWritingStyle(sentence));
+        System.out.println(jadenSmithWritingStyle(sentence));
     }
     public static String jadenSmithWritingStyle(String words){
-        if(words.isEmpty()) return null;
-       // StringBuilder result = new StringBuilder();
-        String result = "";
-        String[] arrayWords = words.toLowerCase().trim().split(" ");
-        for (String word : arrayWords){if (word.isEmpty()) continue;
-           //result.append(word.toUpperCase().charAt(0)).append(word.substring(1)).append(" ");
-            result += word.toUpperCase().charAt(0) + word.substring(1)+" ";
+        try {
+            if (words.isEmpty()) return null;
+            // StringBuilder result = new StringBuilder();
+            String result = "";
+            String[] arrayWords = words.toLowerCase().trim().split(" ");
+            for (String word : arrayWords) {
+                if (word.isEmpty()) continue;
+                //result.append(word.toUpperCase().charAt(0)).append(word.substring(1)).append(" ");
+                result += word.toUpperCase().charAt(0) + word.substring(1) + " ";
+            }
+            //return result.toString().trim();
+            return result.trim();
+        }catch(NullPointerException e){
+            return  "NullPointer Exception";
         }
-        //return result.toString().trim();
-        return result.trim();
     }
 }
