5b2a24a8e2fe9bd09fb4dcb326a6a52eb0e90d08
SHOKHZODJURAEV <85317197+SHOKHZODJURAEV@users.noreply.github.com>
2023-07-02 11:10:00 -0400

    Group solution shared

--- JavaCodingTasks/TaskNumber04/MultiplyOfArrayElements.java
+++ JavaCodingTasks/TaskNumber04/MultiplyOfArrayElements.java
@@ -6,21 +6,21 @@ public class MultiplyOfArrayElements {
 
     public static void main(String[] args) {
 
-        int[] arr = {2,3,4,5,6};
+        int[] arr = {2,3}; //
 
         System.out.println(Arrays.toString(multiplyPreviousNextElementsArray(arr)));
     }
-    public static int[] multiplyPreviousNextElementsArray(int[] array){
-        if(array.length <= 1)return null;
-
-        int[] arrResult = new int[array.length];
-        arrResult[0] = array[0] * array[1];
-        arrResult[array.length-1] = array[array.length-1] * array[array.length-2];
-        for(int i = 1; i < array.length-1; i++ ){
-            arrResult[i] = array[i-1] * array[i+1];
+    public static int[] multiplyPreviousNextElementsArray(int[] array){ // {2,3,4,5,6}
+        if(array == null){
+            throw new IllegalArgumentException("Array can not be null value");
+        }
+        if(array.length <= 1)return array;
+        int[] arrResult = new int[array.length]; // 5 {6,8,15,24,30}
+        arrResult[0] = array[0] * array[1]; //2 * 3 = 6
+        arrResult[array.length-1] = array[array.length-1] * array[array.length-2]; // 30
+        for(int i = 1; i < array.length-1; i++ ){ // i = 2
+            arrResult[i] = array[i-1] * array[i+1]; //2 * 4 = 8;
         }
         return arrResult;
     }
-
-
 }
--- JavaCodingTasks/TaskNumber04/MultiplyOfArrayElementsTest.java
+++ JavaCodingTasks/TaskNumber04/MultiplyOfArrayElementsTest.java
@@ -9,10 +9,41 @@ import java.util.Arrays;
 public class MultiplyOfArrayElementsTest {
 
     @Test
-    void checkMultiplicationInArray(){
+    void checkMultiplicationInArray01(){
 
-        int[] actualResult = {2,3,4,5,6};
+        int[] inputValue = {2,3,4,5,6};
         int[] expectedResult = {6, 8, 15, 24, 30};
-        Assertions.assertEquals(Arrays.toString(expectedResult), Arrays.toString(MultiplyOfArrayElements.multiplyPreviousNextElementsArray(actualResult)));
+        int[] actualResult = MultiplyOfArrayElements.multiplyPreviousNextElementsArray(inputValue);
+     //   Assertions.assertEquals(Arrays.toString(expectedResult), Arrays.toString(actualResult));
+      //  Assertions.assertArrayEquals(expectedResult, actualResult);
+        Assertions.assertTrue(Arrays.equals(expectedResult, actualResult));
+        // Arrays.deepEquals(expectedResult, actualResult); comparing dimentional array and objects
+
+    }
+
+    @Test
+    void checkMultiplicationInArray02(){
+
+        int[] inputValue = {2};
+        int[] expectedResult = {2};
+        int[] actualResult = MultiplyOfArrayElements.multiplyPreviousNextElementsArray(inputValue);
+        //   Assertions.assertEquals(Arrays.toString(expectedResult), Arrays.toString(actualResult));
+        //  Assertions.assertArrayEquals(expectedResult, actualResult);
+        Assertions.assertTrue(Arrays.equals(expectedResult, actualResult));
+        // Arrays.deepEquals(expectedResult, actualResult); comparing dimentional array and objects
+
+    }
+
+    @Test
+    void checkMultiplicationInArray03(){
+
+        int[] inputValue = {2, 3};
+        int[] expectedResult = {6 ,6};
+        int[] actualResult = MultiplyOfArrayElements.multiplyPreviousNextElementsArray(inputValue);
+        //   Assertions.assertEquals(Arrays.toString(expectedResult), Arrays.toString(actualResult));
+        //  Assertions.assertArrayEquals(expectedResult, actualResult);
+        //  Assertions.assertThrows(() -> {} ); assert to exception
+        // Arrays.deepEquals(expectedResult, actualResult); comparing dimentional array and objects
+
     }
 }
